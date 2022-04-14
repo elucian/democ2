d6f27e262f060c837ad5e176bc7e3fc5fe46ff55
Elucian Moise <elucian.moise@gmail.com>
2022-04-14 00:24:56 +0000

    improve main

--- .ccls-cache/@home@runner@democ/main.c
+++ .ccls-cache/@home@runner@democ/main.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 
+int read_menu() {
+  int option;
+  printf("----* menu *----\n");
+  printf("0 - exit        \n");
+  printf("1 - do_demo     \n");
+  printf("----------------\n");
+  printf("run :> ");
+  scanf("%d", &option);  
+  printf("----------------\n");
+  return option;
+}
+
+void do_demo () {
+   // control variable
+  printf("do demo:\n"); 
+   int x = 1;
+   // start loop
+   do {
+      x = x + 1;
+   } while ( x < 4 );
+   // end loop
+  
+   // expect to print x = 4
+   printf("x = %d\n", x); 
+   
+}
+
 int main(void) {
-  printf("Hello World\n");
+  printf("----------------\n");
+  printf("Sage-Code C Demo\n");
+  int option = 1;
+
+  while (option > 0) {
+    option = read_menu();
+    switch (option) {
+      case 1: do_demo();
+    }  
+  }
   return 0;
 }
+
---
+++ .ccls-cache/@home@runner@democ/src@test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int test()
+{
+    int test;
+    printf("Enter number: ");
+    scanf("%d", &test);  
+    printf("Number = %d",test);
+    return 0;
+}
